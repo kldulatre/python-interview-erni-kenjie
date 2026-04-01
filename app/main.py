083c5a7b1d9cf32ae9e7da09166de89adda0d8ee
Kenjie Lloyd Dulatre <kldulatre@gmail.com>
2026-04-01 17:37:39 +0800

    refactor: remove item-related models, schemas, CRUD operations, and API endpoints

--- app/main.py
+++ app/main.py
@@ -24,8 +24,8 @@ if settings.BACKEND_CORS_ORIGINS:
         allow_headers=["*"],
     )
 
-app.include_router(api_router, prefix=settings.API_V1_STR)
-app.include_router(exchange_rate_router, prefix=settings.API_V1_STR)
+# app.include_router(api_router, prefix=settings.API_V1_STR)
+# app.include_router(exchange_rate_router, prefix=settings.API_V1_STR)
 
 @app.get("/")
 def root():
